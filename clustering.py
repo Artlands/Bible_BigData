7d61f96fd33960b8917ade85ba84eaea2084a324
artlands <lijie0416@gmail.com>
2018-10-10 15:20:14 -0500

    k-means clustring using library

--- clustering.py
+++ clustering.py
@@ -54,6 +54,24 @@ class Cluster:
         stems = [stemmer.stem(t) for t in filtered_tokens]
         return stems
 
+    def tokenize_only(self, raw_text):
+        # remove unicode
+        text = "".join([x for x in raw_text if ord(x) < 128])
+        # convert to lower case and split
+        words = text.lower().split()
+        # remove stopwords
+        stopword_set = set(stopwords.words("english"))
+        meaningful_words = [w for w in words if w not in stopword_set]
+        # join the cleaned words in a list
+        cleaned_word_list = " ".join(meaningful_words)
+
+        tokens = [word for sent in nltk.sent_tokenize(cleaned_word_list) for word in nltk.word_tokenize(sent)]
+        filtered_tokens = []
+        for token in tokens:
+            if re.search('[a-zA-Z]', token):
+                filtered_tokens.append(token)
+        return filtered_tokens
+
     def cluster(self):
         start = time.time()
 
@@ -64,9 +82,21 @@ class Cluster:
             documents = data['books']
             texts = data['verses']
 
+        print (f'Tokenizing and stemming words...')
+        totalvocab_stemmed = []
+        totalvocab_tokenized = []
+        for i in texts:
+            allwords_stemmed = self.tokenize_and_stem(i) #for each item in 'synopses', tokenize/stem
+            totalvocab_stemmed.extend(allwords_stemmed) #extend the 'totalvocab_stemmed' list
+
+            allwords_tokenized = self.tokenize_only(i)
+            totalvocab_tokenized.extend(allwords_tokenized)
+        vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index = totalvocab_stemmed)
+        print (f"there are {str(vocab_frame.shape[0])} items in vocab_frame")
+
         # Convert a collection of raw documents to a matrix of TF-IDF features.
         print (f'Creating tfidf matrix...')
-        tfidf_vectorizer = TfidfVectorizer(max_df = 0.95, max_features = 500000, min_df = 0.05, use_idf=True, tokenizer = self.tokenize_and_stem, ngram_range=(1, 3))
+        tfidf_vectorizer = TfidfVectorizer(max_df = 0.90, max_features = 100000, min_df = 0.10, use_idf=True, tokenizer = self.tokenize_and_stem, ngram_range=(1, 3))
         # Apply tfidf normalization to a sparse matrix of occurrence counts
         tfidf_matrix = tfidf_vectorizer.fit_transform(texts)
 
@@ -82,9 +112,68 @@ class Cluster:
         km.fit(tfidf_matrix)
         clusters = km.labels_.tolist()
 
-        books = {'book': dcouments, 'verses': texts}
+        books = {'book': documents, 'verses': texts}
         frame = pd.DataFrame(books, index = [clusters], columns = ['book', 'cluster'])
-        print(frame['cluster'].value_counts())
+
+        order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+
+        for i in range(num_clusters):
+            print(f"Cluster {i} words:")
+            for ind in order_centroids[i, :10]:
+                print(f"{vocab_frame.loc[terms[ind].split(' ')].values.tolist()[0][0]}")
+            print()
+            print(f"Cluster {i} books:")
+            for book in frame.loc[i]['book'].values.tolist():
+                print(f"{book} ")
+            print()
+
+        #Multidimensional scaling
+        from sklearn.manifold import MDS
+        mds = MDS(n_components = 2, dissimilarity="precomputed", random_state = 1)
+        pos = mds.fit_transform(dist) # shape(n_components, n_samples)
+        xs, ys = pos[:, 0], pos[:, 1]
+        #Visualizing document clusters
+        cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e'}
+        cluster_names = {0:'said, thou, him, said, thee, jesus, came, thy, them, went',
+                         1:'christ, jesus, faithful, jesus, you, us, lord, loved, grace, thou',
+                         2:'children, offering, israel, moses, thou, priest, king, house, hundred, people',
+                         3:'king, said, house, thy, david, israel, thou, came, him, solomon',
+                         4: 'thy, thou, thee, saith, saith, people, me, house, land, lord'}
+
+        df = pd.DataFrame(dict(x = xs, y = ys, label=clusters, title = documents))
+        groups = df.groupby('label')
+
+        # set up plot
+        fig, ax = plt.subplots(figsize=(17, 9)) # set size
+        ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+
+        #iterate through groups to layer the plot
+        #note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+        for name, group in groups:
+            ax.plot(group.x, group.y, marker='o', linestyle='', ms=12,
+                    label=cluster_names[name], color=cluster_colors[name],
+                    mec='none')
+            ax.set_aspect('auto')
+            ax.tick_params(\
+                axis = 'x',          # changes apply to the x-axis
+                which ='both',      # both major and minor ticks are affected
+                bottom = False,      # ticks along the bottom edge are off
+                top = False,         # ticks along the top edge are off
+                labelbottom=False)
+            ax.tick_params(\
+                axis= 'y',         # changes apply to the y-axis
+                which='both',      # both major and minor ticks are affected
+                left = False,      # ticks along the bottom edge are off
+                top = False,         # ticks along the top edge are off
+                labelleft=False)
+
+        ax.legend(numpoints=1)  #show legend with only 1 point
+
+        #add label in x,y position with the label as the film title
+        for i in range(len(df)):
+            ax.text(df.loc[i]['x'], df.loc[i]['y'], df.loc[i]['title'], size=8)
+
+        plt.show() #show the plot
 
         #Plot similarity--------------------------------------------------------
         # sns.set(style="white")
